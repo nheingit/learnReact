235b9bff0a022de0a113647b87952bc6288a9c5f
nheingit <noah.hein@g.austincc.edu>
2020-10-12 13:20:53 -0500

    added tweet styling

---
+++ src/Tweet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+function Tweet(){
+    return (
+      <div className="tweet">
+          <h3>Name</h3>
+          <p>this is the tweet</p>
+          <h3>numOfLikes</h3>
+      </div>  
+    );
+}
+export default Tweet;
